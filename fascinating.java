1886bf3ee77397d7e2524c88c1ca2c641a825442
JEEVITHA NANDHAKUMAR <117155007+jeevijinu@users.noreply.github.com>
2023-01-06 13:32:09 +0530

    Add files via upload

---
+++ fascinating.java
@@ -0,0 +1,35 @@
+package jeevi;
+import java.util.*;
+import java.util.*;
+class Fascinating
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter any Number : ");
+        int n = sc.nextInt();
+        int n2 = n * 2;
+        int n3 = n * 3;
+        String con = n + "" + n2 + n3;
+        boolean found = true;
+        for(char i = '1'; i <= '9'; i++)
+        {
+            int count = 0;
+            for(int j = 0; j < con.length(); j++)
+            {
+                char ch = con.charAt(j);
+                if(ch == i)
+                    count++;
+            }
+            if(count > 1 || count == 0)
+            {
+                found = false;
+                break;
+            }
+        }
+        if(found)
+            System.out.println(n + " is a fascinating number.");
+        else
+            System.out.println(n + " is not a fascinating number.");
+    }
+}
